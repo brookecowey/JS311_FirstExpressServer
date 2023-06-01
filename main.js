2bf421be04a5e1c1d06ded1312dd2c5c10d68829
brookecowey <brooke.cowey@gmail.com>
2023-05-31 19:13:58 -0500

    added js

---
+++ main.js
@@ -0,0 +1,22 @@
+let express = require("express");
+
+let app = express()
+
+app.get(num1, num2)
+let num1 = 5;
+let num2 = 6;
+
+res.send(11)
+
+
+app.post(num3, num4)
+let num3 = 3;
+let num4 = 12;
+
+res.send(36)
+
+app.get(num5)
+
+let num5 = 4;
+
+res.send(-4)
